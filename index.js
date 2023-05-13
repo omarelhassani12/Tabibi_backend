606f7f077af477196dd21b68f3d1941a973f5537
Omar EL HASSANI <omarelhassani33@gmail.com>
2023-05-13 17:58:45 +0100

    add the register and login for the user with all roles

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const express = require('express');
 const app = express();
 
 // Import the database connection object from the db.js file
-const db = require('./db.js');
 
 // Set up a route handler for the root URL
-// app.get('/', (req, res) => {
-//   res.send('Welcome')
-// });
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
 
 // Use the express.json() middleware to parse incoming JSON requests
 app.use(express.json());
 
 // Use body-parser middleware to parse incoming URL-encoded form data
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true  , limit : "8mb"}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "8mb" }));
 app.use(bodyParser.json());
 
 // Import the user router and register it with the app
@@ -23,6 +22,6 @@ const userRouter = require('./user');
 app.use('/user', userRouter);
 
 // Start the Express app and listen on port 3000
-app.listen(3000, () => {
+app.listen(3000, "192.168.1.6", () => {
   console.log('Server is running on port 3000');
 });
--- user.js
+++ user.js
@@ -1,51 +1,101 @@
 const express = require('express');
 const router = express.Router();
-var db = require('./db.js');
+const db = require('./db.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
-// Define a route handler for the '/register' endpoint that accepts POST requests
 router.post('/register', (req, res) => {
     // Extract data from the request body
-    var name = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
+    const { type, nomPrenom, email, cni, password, telephone, age, urgence, sexe } = req.body;
 
-    console.log(name);
-    // Create a SQL query to insert a new user into the database
-    var sqlQuery = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    // Check if the username field is not empty
+    if (!nomPrenom) {
+        res.status(400).json({ success: false, message: 'Username field cannot be empty' });
+        return;
+    }
 
-    // Call the database to execute the query
-    db.query(sqlQuery, [name, email, password], function(error, data, fields) {
-        if (error) {
-            // If there was an error, send an error response
-            res.json({ success: false, message: error });
+    // Hash the password before storing it in the database
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) {
+            // If there's an error hashing the password, send an error response
+            console.error(err);
+            res.status(500).json({ success: false, message: 'Internal server error' });
         } else {
-            // If the query was successful, send a success response
-            // console.log({ success: true, message: 'register' });
-             res.json({ success: true, message: 'register' });
+            // Create a SQL query to insert a new user into the database
+            const sqlQuery = "INSERT INTO users (role, username, email, cni, password, phone, age, urgence, sexe) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            // Call the database to execute the query
+            db.query(sqlQuery, ['Patient', nomPrenom, email, cni, hashedPassword, telephone, age, urgence, sexe], function(error, data, fields) {
+                if (error) {
+                    // If there was an error inserting the user, send an error response
+                    console.error(error);
+                    res.status(500).json({ success: false, message: 'Internal server error' });
+                } else {
+                    // If the query was successful, send a success response
+                    res.json({ success: true, message: 'User registered successfully' });
+                }
+            });
         }
     });
-    console.log(name);
 });
-// Define a route handler for the '/login' endpoint that accepts POST requests
-router.route('/login').post((req,res)=>{
-    var email = req.body.email;
-    var password = req.body.password;
 
-    var sql = "SELECT * FROM users WHERE email=? AND password=?";
-    console.log(email);
-    //write engin
-    db.query(sql,[email,password],function(err,data,fields){
-        if(err){
-            res.json({ success: false, message: err });
-        }else{
-            if(data.length > 0){
-                res.json({ success: true, message: data });
-                console.log(data);
-            }else{
-                res.json({ success: false, message: 'Empty Data' });
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    // Query the database for the user with the given email
+    const sqlQuery = "SELECT * FROM users WHERE email=?";
+    db.query(sqlQuery, [email], function(error, data, fields) {
+        if (error) {
+            res.json({ success: false, message: error });
+        } else {
+            if (data.length > 0) {
+                // Compare the password with the input password
+                bcrypt.compare(password, data[0].password, function(err, result) {
+                    if (result) {
+                        // User authenticated, generate JWT token and send back to client
+                        const token = jwt.sign({ email }, 'my-secret-key');
+                        res.json({ success: true, data , token });
+                    } else {
+                        res.json({ success: false, message: 'Invalid email or password' });
+                    }
+                });
+            } else {
+                res.json({ success: false, message: 'Invalid email or password' });
             }
         }
     });
 });
 
+
+
+// // Define a route handler for the '/login' endpoint that accepts POST requests
+// router.post('/login', (req, res) => {
+//     const { email, password } = req.body;
+
+//     // Query the database for the user with the given email
+//     const sqlQuery = "SELECT * FROM users WHERE email=?";
+//     db.query(sqlQuery, [email], function(error, data, fields) {
+//         if (error) {
+//             res.json({ success: false, message: error });
+//         } else {
+//             if (data.length > 0) {
+//                 // Compare the password with the input password
+//                 if (password === data[0].password) {
+//                     // User authenticated, generate JWT token and send back to client
+//                     const token = jwt.sign({ email }, 'my-secret-key');
+//                     res.json({ success: true, data , token });
+//                 } else {
+//                     res.json({ success: false, message: 'Invalid email or password' });
+//                 }
+//             } else {
+//                 res.json({ success: false, message: 'Invalid email or password' });
+//             }
+//         }
+//     });
+// });
+
+
+  
+  
 module.exports = router;
